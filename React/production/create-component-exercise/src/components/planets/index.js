1fab7d6bdf7bf87d5d0e0b289b3219151175e846
João Lucas <lucasmonteirolima17@gmail.com>
2021-08-13 12:11:17 -0300

    Renderizando multiplos components

--- React/production/create-component-exercise/src/components/planets/index.js
+++ React/production/create-component-exercise/src/components/planets/index.js
@@ -1,20 +1,30 @@
 import Planet from './planet';
 import React, { Fragment } from 'react';
 
+const clickOnPlanet = (name) =>{
+    console.log(`Um click no Planeta: ${name}`)
+}
+
 const Planets = () => {
     return (
         <Fragment>
             <h3>Planet List</h3>
+            <button >Show Message!</button>
             <hr />
             <Planet name="Mercúrio"
                 text="Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. Mercúrio completa três rotações em torno de seu eixo a cada duas órbitas. "
                 url="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
                 img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
+                clickOnPlanet={clickOnPlanet}
+                title_with_underline = {true}
+                gray={true}
             />
             <Planet name="Plutão"
                 text="Plutão, formalmente designado 134340 Plutão, é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol. Originalmente classificado como um planeta, Plutão é atualmente o maior membro conhecido do cinturão de Kuiper,[12] uma região de corpos além da órbita de Netuno."
                 url="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
                 img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
+                clickOnPlanet={clickOnPlanet}
+                gray={true}
             />
         </Fragment >
     )
--- React/production/create-component-exercise/src/components/planets/planet/index.js
+++ React/production/create-component-exercise/src/components/planets/planet/index.js
@@ -3,11 +3,28 @@ import GrayImg from '../../shared/gray_img';
 import DescriptionWithLink from '../../shared/description_with_link';
 
 const Planet = (props) => {
+
+    let title;
+    if (props.title_with_underline)
+        title = <h4><u>{props.name}</u></h4>
+    else
+        title = <h4>{props.name}</h4>
+
     return (
-        <div>
-            <h4>{props.name}</h4>
+        <div onClick={() => props.clickOnPlanet(props.name)}>
+            {title}
             <DescriptionWithLink text={props.text} url={props.url} />
-            <GrayImg img_url={props.img_url} />
+            <GrayImg img_url={props.img_url} gray={props.gray} />
+
+            <h4>Satélites</h4>
+            <ul>
+                {
+                    ['a', 'b', 'c', 'd'].map((n) =>
+                        <li>Satélite {n}</li>
+                    )
+                }
+            </ul>
+            <hr />
         </div>
     )
 }
