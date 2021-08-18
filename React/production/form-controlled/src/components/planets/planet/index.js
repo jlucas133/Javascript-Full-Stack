1c5740cd199ef3004c53b632a3d10586fd66762f
Joao Lucas <lucasmonteirolima17@gmail.com>
2021-08-18 09:21:42 -0300

    Form exercicio

--- React/production/form-controlled/src/components/planets/planet/index.js
+++ React/production/form-controlled/src/components/planets/planet/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import GrayImg from '../../shared/gray_img';
 import DescriptionWithLink from '../../shared/description_with_link';
+import Form from './form';
 
 async function getSatellites(id) {
     let response = await fetch(`http://localhost:3000/api/${id}.json`)
@@ -18,6 +19,10 @@ const Planet = (props) => {
         })
     }, [])
 
+    const addSatellites = (new_satellite) => {
+        setSatellites([...satellites, new_satellite])
+    }
+
     let title;
     title = <h4>{props.name}</h4>
     return (
@@ -26,12 +31,14 @@ const Planet = (props) => {
             <DescriptionWithLink description={props.description} link={props.link} />
             <GrayImg img_url={props.img_url} gray={props.gray} />
             <h4>Satélites</h4>
+            <hr/>
+                <Form addSatellites={addSatellites}/>
+            <hr/>
             <ul>
                 {satellites.map((satellite, index) =>
                     <li key={index}>{satellite.name}</li>
                 )}
             </ul>
-
             <hr />
         </div>
     )
