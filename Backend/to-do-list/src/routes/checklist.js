fdd5c3266e2b76f154d64f455ec5a9227d8fd975
Lucas Lima <lucasmonteirolima17@gmail.com>
2021-08-20 11:24:46 -0300

    Parametros nas rotas

--- Backend/to-do-list/src/routes/checklist.js
+++ Backend/to-do-list/src/routes/checklist.js
@@ -11,4 +11,9 @@ router.post('/', (req, res)=>{
     console.log(req.body)
     res.status(200).json(req.body);
 })
+
+router.get('/:id', (req, res)=>{
+    console.log(req.params.id);
+    res.send(`ID: ${req.params.id}`);
+})
 module.exports = router;
