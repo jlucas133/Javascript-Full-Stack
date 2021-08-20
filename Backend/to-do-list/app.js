8ee174847de840725dc204443809198fad3c9e49
Lucas Lima <lucasmonteirolima17@gmail.com>
2021-08-20 10:36:41 -0300

    Adicionando nova rota

--- Backend/to-do-list/app.js
+++ Backend/to-do-list/app.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/', (req, res) =>{
     res.send('<h1>Minha lista de Tarefas :)</h1>');
 })
+app.get('/json', (req,res)=>{
+    res.json({title: 'Tarefa X', done: true})
+});
 
 app.listen(3000,()=>{
     console.log('Servidor iniciado')
