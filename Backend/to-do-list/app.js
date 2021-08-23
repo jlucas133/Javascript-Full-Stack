dd98082183acf8c4c27199ced7bf9ec7e7b2df04
Lucas Lima <lucasmonteirolima17@gmail.com>
2021-08-23 16:12:08 -0300

    Adicionando tasks na checklist

--- Backend/to-do-list/app.js
+++ Backend/to-do-list/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const checkListRouter = require('./src/routes/checklist');
+const taskRouter = require('./src/routes/task');
 const rootRouter = require('./src/routes/index');
+
 const methodOverride = require('method-override')
 require('./config/database');
 
@@ -21,6 +23,10 @@ app.set('view engine', 'ejs');
 
 app.use('/',rootRouter);
 app.use('/checklists',checkListRouter);
+app.use('/tasks',taskRouter.simple);
+
+app.use('/checklists',taskRouter.checklistDepedent);
+
 
 app.listen(3000,()=>{
     console.log('Servidor iniciado')
